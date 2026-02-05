e6ca5e0019a3155f3f3cb4cc7e1c230d99238643
Saroj-kr-tharu <sarojkumartharu11234@gmail.com>
2026-02-05 19:39:17 +0545

    added

--- college-management-system-backend/build/types/global.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.allAST = exports.allAdminsTeachers = exports.onlyTeacher = exports.onlyStudent = exports.onlyAdmin = void 0;
-const enum_types_1 = require("./enum.types");
-exports.onlyAdmin = [enum_types_1.Role.ADMIN];
-exports.onlyStudent = [enum_types_1.Role.STUDENT];
-exports.onlyTeacher = [enum_types_1.Role.TEACHER];
-exports.allAdminsTeachers = [...exports.onlyAdmin, ...exports.onlyTeacher];
-exports.allAST = [...exports.onlyAdmin, ...exports.onlyStudent, ...exports.onlyTeacher];
